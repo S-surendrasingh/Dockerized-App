b2b96a3f1bb0246182720fcec94c7aace3411d2a
S-surendrasingh <surendrarajput7546@gmail.com>
2024-11-26 18:05:09 +0530

    Add protect_from_forgery in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# ApplicationController serves as the base controller for all other controllers
+# in the application. It contains common behavior such as CSRF protection
+# and session handling. All other controllers in the app inherit from this class.
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
